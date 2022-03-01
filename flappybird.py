384239bba4230b481e421818fbe521565af142ad
JoakimCewers <joakim.cewers@gmail.com>
2022-03-01 22:12:01 +0100

    Update flappybird.py

--- flappybird.py
+++ flappybird.py
@@ -9,33 +9,6 @@ FPS = 120
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 BLACK = (0,0,0)
 
-def drawWindow():
-    WIN.fill((255,255,255))
-    pygame.display.update()
-    pygame.draw.circle(WIN,BLACK,mousePos(),20)
-def main():
-    
-    player = Bird(100,HEIGHT/2)
-    clock = pygame.time.Clock()
-    run = True
-    while run:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
-
-            if event.type == pygame.QUIT:
-                run = False
-
-        drawWindow()
-        
-        
-    pygame.quit()
-
-def mousePos():
-    pos = pygame.mouse.get_pos()
-    return pos
 
 
 
@@ -51,6 +24,39 @@ class Bird():
         
         pygame.draw.circle(WIN,BLACK,(self.x,self.y),20)
     
+
+def drawWindow(player = Bird):
+    WIN.fill((255,255,255))
+    player.draw()
+    pygame.display.update()
+    
+def main():
+    
+    player = Bird(100,HEIGHT/2)
+    clock = pygame.time.Clock()
+    run = True
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pass
+
+            if event.type == pygame.QUIT:
+                run = False
+
+        drawWindow(player)
+        
+        
+    pygame.quit()
+
+def mousePos():
+    pos = pygame.mouse.get_pos()
+    return pos
+
+
+
+
 
 if __name__ == "__main__":
     
